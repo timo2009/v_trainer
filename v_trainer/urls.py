8c5f2ba77da1cdaafc673c1fa385620a912828a2
timo2009 <timo@tomsbox.de>
2021-02-19 17:43:15 +0100

    englischtesteinevokabel_detail.html erstellt

--- v_trainer/urls.py
+++ v_trainer/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
 	path('', IndexView.as_view(), name='index'),
 	path('englischtest_1_vokabel/create/', EnglischTestEineVokalelCreate.as_view(), name='englischtest_1_vokabel_create'),
 	path('englischtest_1_vokabel/update/<pk>/', EnglischTestEineVokalelUpdate.as_view(), name='englischtest_1_vokabel_update'),
+	path('englischtest_1_vokabel/detail/<pk>/', EnglischTestEineVokalelDetail.as_view(), name='englischtest_1_vokabel_detail'),
 	path('englischtest_1_vokabel/list/', EnglischTestEineVokalelList.as_view(), name='englischtest_1_vokabel_list'),
 ]
--- v_trainer/views/vokalbeltest_englisch_1_vokabel.py
+++ v_trainer/views/vokalbeltest_englisch_1_vokabel.py
@@ -1,7 +1,9 @@
 from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
-from v_trainer.models import EnglischTestEineVokalel
+from v_trainer.models import EnglischTestEineVokalel, DeutschesWort
 from django.urls import reverse_lazy
+from django.utils import timezone
 
 
 class EnglischTestEineVokalelCreate(CreateView):
@@ -21,3 +23,15 @@ class EnglischTestEineVokalelUpdate(UpdateView):
 
 class EnglischTestEineVokalelList(ListView):
     model = EnglischTestEineVokalel
+
+
+class EnglischTestEineVokalelDetail(DetailView):
+    model = EnglischTestEineVokalel
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['all_dwort_list'] = DeutschesWort.objects.all()
+        context['test'] = DeutschesWort.objects.filter(wort=EnglischTestEineVokalel.antwort_englisches_wort_1)
+        # context['all_dwort_list'] = DeutschesWort.filter(products__product_company=company)
+        return context
+
