1c6b439d8045ad94c1f5c80b675e622adb7f7a30
timo2009 <timo@tomsbox.de>
2021-06-04 15:43:04 +0200

    Verschönerung_9

--- radioready/models.py
+++ radioready/models.py
@@ -6,7 +6,7 @@ class RadioShow(models.Model):
     name = models.CharField(max_length=100, default='')
     link_to_radio_show = models.URLField()
     quiz = models.CharField(max_length=1000, default='', blank=True)
-    einsendeschluss = models.DateTimeField(auto_now=False, auto_now_add=False, default='', blank=True, null=True)
+    einsendeschluss = models.CharFieldField(max_length=1000, default='', blank=True)
 
     def __str__(self):
         return self.name
