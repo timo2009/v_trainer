010501aff79ba936727c48178778cd7db43e4fbe
timo2009 <timo@tomsbox.de>
2021-06-19 07:34:07 +0200

    Quiz text field

--- radioready/models.py
+++ radioready/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class RadioShow(models.Model):
     name = models.CharField(max_length=100, default='')
     link_to_radio_show = models.URLField()
-    quiz = models.CharField(max_length=1000, default='', blank=True)
+    quiz = models.TextField(max_length=1000, default='', blank=True)
     winner = models.CharField(max_length=1000, default='', blank=True)
     deadline = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
     created = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
