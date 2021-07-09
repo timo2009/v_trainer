3a089c53198f7e65ae36341c06b46a82ab89f893
timo2009 <timo@tomsbox.de>
2021-07-09 14:42:53 +0200

    Deutschtest im admin vorhanden

--- v_trainer/admin.py
+++ v_trainer/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from v_trainer.models import DeutschesWort, EnglischesWort, EnglischTestEineVokalel, DeutschTestEineVokalel, LateinTestZehnVokalel, LateinischesWort, EnglischTestZehnVokalel
+from v_trainer.models import DeutschesWort, EnglischesWort, EnglischTestEineVokalel, DeutschTestEineVokalel, LateinTestZehnVokalel, LateinischesWort, EnglischTestZehnVokalel, DeutschTestZehnVokalel
 
 admin.site.register(DeutschesWort)
 admin.site.register(EnglischesWort)
@@ -23,4 +23,8 @@ class LateinTest(admin.ModelAdmin):
 @admin.register(EnglischTestZehnVokalel)
 class EnglischTest(admin.ModelAdmin):
     list_display = ("id", "user", "englisches_wort_1", "englisches_wort_2", "englisches_wort_3", "englisches_wort_4", "englisches_wort_5", "englisches_wort_6", "englisches_wort_7", "englisches_wort_8", "englisches_wort_9", "englisches_wort_10")
+
+@admin.register(DeutschTestZehnVokalel)
+class EnglischTest(admin.ModelAdmin):
+    list_display = ("id", "user", "deutsches_wort_1", "deutsches_wort_2", "deutsches_wort_3", "deutsches_wort_4", "deutsches_wort_5", "deutsches_wort_6", "deutsches_wort_7", "deutsches_wort_8", "deutsches_wort_9", "deutsches_wort_10")
 
